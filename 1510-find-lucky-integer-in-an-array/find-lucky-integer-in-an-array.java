f36f2020170586d3bf0266a2aab585eb203a5b04
Shaurya Bansal <129157553+ShauryaB24@users.noreply.github.com>
2025-02-04 21:20:11 +0530

    Time: 1 ms (100.00%) | Memory: 43.1 MB (83.11%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findLucky(int[] arr) {
         int[] count = new int[501];
-        int preans = -1;
+        //int preans = -1;
         int ans = -1;
         for(int num:arr) {
             count[num]++;
         }
 
-        for(int num:arr) {
-            if(count[num] == num)preans = num;
-
-            ans = Math.max(ans, preans);
+        for(int i=1;i<=500;i++){
+            if(count[i] == i)
+                ans = i;
         }
+        
 
         return ans;
     }
